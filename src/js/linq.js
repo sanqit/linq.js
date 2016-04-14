e6704be5601f77808c1e4240609cf2ccfa096b81
sanqit <admin@sanqit.ru>
2016-04-14 20:25:03 +0300

    add max and min queries

--- src/js/linq.js
+++ src/js/linq.js
@@ -9,8 +9,7 @@
 Object.defineProperty(Object.prototype, "equals", {
 	value: function (b) {
 		"use strict";
-		var a = this;
-		return a === b || a === b || JSON.stringify(a) === JSON.stringify(b);
+		return this === b || JSON.stringify(this) === JSON.stringify(b);
 	}
 });
 
@@ -20,8 +19,7 @@ Object.defineProperty(Array.prototype, "select", {
 	value: function(selector) {
 		"use strict";
 		var arr = [];
-		var length = this.length;
-		for (var i = 0; i < length; i++)
+		for (var i = 0; i < this.length; i++)
 			arr.push(selector(this[i]));
 		return arr;
 	}
@@ -31,8 +29,7 @@ Object.defineProperty(Array.prototype, "where", {
 	value: function (predicate) {
 		"use strict";
 		var arr = [];
-		var length = this.length;
-		for (var i = 0; i < length; i++)
+		for (var i = 0; i < this.length; i++)
 			if (predicate(this[i]) === true) arr.push(this[i]);
 		return arr;
 	}
@@ -42,8 +39,7 @@ Object.defineProperty(Array.prototype, "any", {
 	value: function (predicate) {
 		"use strict";
 		if (predicate == null) return this.length !== 0;
-		var length = this.length;
-		for (var i = 0; i < length; i++)
+		for (var i = 0; i < this.length; i++)
 			if (predicate(this[i]) === true) return true;
 		return false;
 	}
@@ -53,8 +49,7 @@ Object.defineProperty(Array.prototype, "all", {
 	value: function (predicate) {
 		"use strict";
 		if (predicate == null) return this.length !== 0;
-		var length = this.length;
-		for (var i = 0; i < length; i++)
+		for (var i = 0; i < this.length; i++)
 			if (predicate(this[i]) !== true) return false;
 		return true;
 	}
@@ -64,10 +59,9 @@ Object.defineProperty(Array.prototype, "orderBy", {
 	value: function (selector, comparer) {
 		"use strict";
 		comparer = comparer || defaultComparer;
-		var fn = function(a, b) {
+		return this.sort(function(a, b) {
 			return comparer(selector(a), selector(b));
-		};
-		return this.sort(fn);
+		});
 	}
 });
 
@@ -83,8 +77,7 @@ Object.defineProperty(Array.prototype, "distinct", {
 	value: function () {
 		"use strict";
 		var arr = [];
-		var length = this.length;
-		for (var i = 0; i < length; i++) {
+		for (var i = 0; i < this.length; i++) {
 			var a = this[i];
 			if (!arr.any(function(x) { return x.equals(a); })) arr.push(a);
 		}
@@ -102,5 +95,25 @@ Object.defineProperty(Array.prototype, "toDictionary", {
 			dictionary[key] = valueSelector != null ? valueSelector(this[i]) : this[i];
 		}
 		return dictionary;
+	}
+});
+
+Object.defineProperty(Array.prototype, "max", {
+	value: function () {
+		"use strict";
+		var max = this[0];
+		for (var i = 1; i < this.length; i++)
+			if (this[i] != null && max < this[i]) max = this[i];
+		return max;
+	}
+});
+
+Object.defineProperty(Array.prototype, "min", {
+	value: function () {
+		"use strict";
+		var min = this[0];
+		for (var i = 1; i < this.length; i++)
+			if (this[i] != null && min > this[i]) min = this[i];
+		return min != null ? min : null;
 	}
 });
--- src/test/test.js
+++ src/test/test.js
@@ -81,14 +81,20 @@ sampleArrayObjAfterToDictionary_valueNull[3] = { SampleID: 3, Desc: "Desc3", Cod
 sampleArrayObjAfterToDictionary_valueNull[4] = { SampleID: 4, Desc: "Desc4", Code: "Code4" };
 sampleArrayObjAfterToDictionary_valueNull[5] = { SampleID: 5, Desc: "Desc5", Code: "Code5" };
 
-function TestEquals() {
+var minMaxTestSample1 = [1, 2, 3, 4, 5, 3, 2, 1, 0, -4];
+
+var minMaxTestSample2 = [-1, undefined, -3, null, -5, -3, -2, -1, 0, -4];
+
+var minMaxTestSample3 = [null, undefined, null];
+
+function testEquals() {
 	var isEquals = true;
 	isEquals &= testSample1.equals(testSample1);
 	isEquals &= [].equals([]);
 	return isEquals;
 }
 
-function TestSelect() {
+function testSelect() {
 	var isSelectComplete = true;
 	isSelectComplete &= (testSampleAfterSelect.equals(testSample1.select(x => x.SampleID)));
 	isSelectComplete &= (testSampleAfterSelect.equals(testSample1.select(x => x.SampleID)));
@@ -97,7 +103,7 @@ function TestSelect() {
 	return isSelectComplete;
 }
 
-function TestWhere() {
+function testWhere() {
 	var isWhereComplete = true;
 	isWhereComplete &= (testSample1.where(x => x.SampleID == 3).equals([{ SampleID: 3, Desc: "Desc3", Code: "Code3" }]));
 	isWhereComplete &= (testSample1.where(x => x.SampleID >= 3).equals([{ SampleID: 4, Desc: "Desc4", Code: "Code4" }, { SampleID: 3, Desc: "Desc3", Code: "Code3" }, { SampleID: 5, Desc: "Desc5", Code: "Code5" }]));
@@ -105,7 +111,7 @@ function TestWhere() {
 	return isWhereComplete;
 }
 
-function TestAny() {
+function testAny() {
 	var isAnyComplete = true;
 	isAnyComplete &= !empty.any();
 	isAnyComplete &= testSample1.any();
@@ -115,7 +121,7 @@ function TestAny() {
 	return isAnyComplete;
 }
 
-function TestAll() {
+function testAll() {
 	var isAllComplete = true;
 	isAllComplete &= testSample4.all(x => x.Desc == "Desc1");
 	isAllComplete &= testSample4.all(x => x.Desc == "Desc1" && x.Code !== "code");
@@ -123,7 +129,7 @@ function TestAll() {
 	return isAllComplete;
 }
 
-function TestDistinct() {
+function testDistinct() {
 	var isDistinctComplete = true;
 	isDistinctComplete &= (testAfterDistinct1.equals(testBeforeDistinct1.distinct(x => x.SampleID)));
 	isDistinctComplete &= (testSample1.equals(testSample1.distinct(x => x.Desc)));
@@ -131,7 +137,7 @@ function TestDistinct() {
 	return isDistinctComplete;
 }
 
-function TestOrderBy() {
+function testOrderBy() {
 	var isOrderByComplete = true;
 	isOrderByComplete &= (testSample2.equals(testSample1.orderBy(x => x.SampleID)));
 	isOrderByComplete &= (testSample2.equals(testSample1.orderBy(x => x.Desc)));
@@ -140,7 +146,7 @@ function TestOrderBy() {
 	return isOrderByComplete;
 }
 
-function TestOrderByDescending() {
+function testOrderByDescending() {
 	var isOrderByDescendingComplete = true;
 	isOrderByDescendingComplete &= (testSample3.equals(testSample1.orderByDescending(x => x.SampleID)));
 	isOrderByDescendingComplete &= (testSample3.equals(testSample1.orderByDescending(x => x.Desc)));
@@ -148,9 +154,25 @@ function TestOrderByDescending() {
 	return isOrderByDescendingComplete;
 }
 
-function TestToDictionary() {
+function testToDictionary() {
 	var isToDictionaryComplete = true;
 	isToDictionaryComplete &= (sampleArrayObjAfterToDictionary.equals(sampleBeforeDistinctArrayObj.toDictionary(x => x.SampleID, x => x.Code)));
 	isToDictionaryComplete &= (sampleArrayObjAfterToDictionary_valueNull.equals(sampleBeforeDistinctArrayObj.toDictionary(x => x.SampleID)));
 	return isToDictionaryComplete;
+}
+
+function testMax() {
+	var isMaxComplete = true;
+	isMaxComplete &= minMaxTestSample1.max() === 5;
+	isMaxComplete &= minMaxTestSample2.max() === 0;
+	isMaxComplete &= minMaxTestSample3.max() == null;
+	return isMaxComplete;
+}
+
+function testMin() {
+	var isMinComplete = true;
+	isMinComplete &= minMaxTestSample1.min() === -4;
+	isMinComplete &= minMaxTestSample2.min() === -5;
+	isMinComplete &= minMaxTestSample3.min() == null;
+	return isMinComplete;
 }
